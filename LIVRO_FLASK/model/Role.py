750728b33ce434eea1f854e7b30a7168ef8a289c
Marcelo Perini <67985323+marceloprni@users.noreply.github.com>
2021-10-25 08:18:05 -0300

    TABELA
    Terminado de montar a tabela

---
+++ LIVRO_FLASK/model/Role.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+from config import app_active, app_config
+
+config = app_config[app_active]
+db = SQLAlchemy(config.APP)
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True, nullable=False)
