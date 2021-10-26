93dc2e18d78129698c5b416ad5f8a7fa8704407d
Marcelo Perini <67985323+marceloprni@users.noreply.github.com>
2021-10-26 08:13:51 -0300

    mudando sql
    mudando sql

--- LIVRO_FLASK/model/User.py
+++ LIVRO_FLASK/model/User.py
@@ -8,7 +8,7 @@ db = SQLAlchemy(config.APP)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(200, unique=True, nullable=False))
+    username = db.Column(db.String(200), unique=True, nullable=False)
     email = db.Column(db.String(200), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.Datetime(6), default=db.func.current_timestamp(), nullable=False)
