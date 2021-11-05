0709a44001bf5fd6d04d49906d6c8ac30c6e334b
Marcelo Perini <67985323+marceloprni@users.noreply.github.com>
2021-11-05 08:11:50 -0300

    Atualizar app
    foi realizado atualização do app

--- LIVRO_FLASK/app.py
+++ LIVRO_FLASK/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 #CONFIG IMPORT
 from config import app_config, app_active
 config = app_config[app_active]
@@ -30,8 +30,27 @@ def create_app(config_name):
     def recovery_password():
         return 'Aqui entrar a tela de recuperar senha'
     
-    @app.route('/profile/<int:id>/')
-    def profile(id):
-        return 'O ID desse usuário é %d' % id
+    @app.route('/profile/<int:id>/action/<action>/')
+    def profile(id, action):
+        if action == 'action1':
+            return 'Ação action1 usuario de ID %d' % id
+        elif action == 'action2':
+            return 'Ação action2 usuario de ID %d' % id 
+        elif action == 'action3':
+            return 'Ação action3 usuario de ID %d' % id
+    
+    @app.route('/profile', methods=['POST'])
+    def create_profile():
+        username = request.form['username']
+        password = request.form['password']
+
+        return 'Essa rota possui um metodo POST e criará um usuario com os dados de usuario %s e senha %s' % (username, password)
+    
+    @app.route('/profile/<int:id>', methods=['PUT'])
+    def edit_total_profile(id):
+        username = request.form['username']
+        password = request.form['password']
+
+        return 'Essa rota possui um metodo PUT e editará o nome do usuário para %s e a senha para %s' % (username, password)
 
     return app
